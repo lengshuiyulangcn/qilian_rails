56223edacc6cff2b2799082b03e84b890fd87f35
lengshuiyulangcn <li.ning@i.mbox.nagoya-u.ac.jp>
2015-09-20 16:04:11 +0900

    before add schedule content

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -18,8 +18,21 @@ class CoursesController < ApplicationController
   def show
     @course = Course.find(params.permit(:id)[:id])
   end
+  def edit
+    @course = Course.find(params.permit(:id)[:id])
+  end
+  def update
+    @course = Course.find(course_params[:id])
+    @course.attributes = course_params
+    if @course.save
+      redirect_to course_path(@course)
+    else
+      redirect_to :back
+    end
+  end
+
   private
   def course_params
-    params.require(:course).permit(:name, :image, :price, :price_detail, :description, :content)
+    params.require(:course).permit(:id, :name, :image, :price, :price_detail, :description, :content)
   end
 end
