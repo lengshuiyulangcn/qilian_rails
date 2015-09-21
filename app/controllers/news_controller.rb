625c295a0967546730c11902fef884dfdd3c4e80
lengshuiyulangcn <li.ning@i.mbox.nagoya-u.ac.jp>
2015-09-21 23:56:10 +0900

    before add impressionist

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,5 +1,7 @@
 class NewsController < ApplicationController
   def show
     @post = Post.find(params.permit(:id)[:id])
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+    @post.content = markdown.render(@post.content)
   end
 end
