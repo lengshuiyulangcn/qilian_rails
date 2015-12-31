a28e09ccb3ee205081c4ea0401aa91355c2ecc09
gyorou <gyorou@tjjtds.me>
2015-12-31 14:06:49 +0900

    modify end of month

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ActiveRecord::Base
   has_and_belongs_to_many :users
 
   def self.coming_events
-    self.where(timestart: Date.today..Date.today.next_month.beginning_of_month )
+    self.where(timestart: Date.today..Date.today.next_month.end_of_month )
   end
   def self.full_calendar_event
     Event.all.map{|event| {id: event.id, title: event.title, start: event.timestart, url: "/event/#{event.id}"}} 
