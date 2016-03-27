c1a61d2fcb628d0601cd4a05194aaabffdd126f8
gyorou <gyorou@tjjtds.me>
2016-03-27 15:10:03 +0900

    rspec config

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -8,12 +8,11 @@ describe EventsController do
     let(:comming_event_params)  { FactoryGirl.attributes_for(:comming_event) }
     let(:comming_event_params_2)  { FactoryGirl.attributes_for(:comming_event_2) }
     let(:held_event)  { FactoryGirl.create(:held_event) }
-    context 'when the current user is normal user' do
+    context 'when the current user is non-login user' do
       before do
-        allow(request.env['warden']).to receive(:authenticate!).and_return(normal_user)
-        allow(controller).to receive(:current_user).and_return normal_user
+        allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, {:scope => :user})
+        allow(controller).to receive(:current_user).and_return(nil)
       end
-      context 'normal user can see the event and the event list' do
         it 'is expected not to access index page' do
           get :index
           expect(response.status).to eq 302 
@@ -34,6 +33,63 @@ describe EventsController do
           expect(Event.all).to include comming_event 
           delete :destroy, id: comming_event.id
           expect(Event.all).to include comming_event 
+        end
+        it 'is expected to be able to see comming event list' do
+          get :list
+          expect(response.status).to eq 200
+          expect(assigns(:events)).to include comming_event
+          expect(assigns(:events)).not_to include held_event 
+        end
+        it 'is expected to be able to see event detail' do
+          get :detail, id: comming_event.id
+          expect(response.status).to eq 200
+        end
+        context 'users are not able to apply for event' do
+          before do
+            request.env["HTTP_REFERER"] = '/'
+          end
+          it 'is expected to not be able to apply for event' do
+            post :apply, event_id: comming_event.id
+            expect(response.status).to eq 302 
+          end
+        end
+    end
+    context 'when the current user is normal user' do
+      before do
+        allow(request.env['warden']).to receive(:authenticate!).and_return(normal_user)
+        allow(controller).to receive(:current_user).and_return normal_user
+      end
+      context 'normal user can not see the event and the event index' do
+        it 'is expected not to access index page' do
+          get :index
+          expect(response.status).to eq 302 
+        end
+        it 'is expected not to access show page' do
+          get :show, id: comming_event.id
+          expect(response.status).to eq 302 
+        end
+        it 'is expected not to create event' do
+          post :create, event: comming_event_params
+          expect(assigns(:event)).to eq nil
+        end
+        it 'is expected not to update event' do
+          patch :update, id: comming_event.id, event: comming_event_params_2
+          expect(assigns(:event)).to eq nil
+        end
+        it 'is expected not to delete event' do
+          expect(Event.all).to include comming_event 
+          delete :destroy, id: comming_event.id
+          expect(Event.all).to include comming_event 
+        end
+        it 'is expected to be able to see comming event list' do
+          get :list
+          expect(response.status).to eq 200
+          expect(assigns(:events)).to include comming_event
+          expect(assigns(:events)).not_to include held_event 
+        end
+        it 'is expected to be able to see event detail' do
+          get :detail, id: comming_event.id
+          expect(response.status).to eq 200
         end
         context 'users are able to apply for event' do
           before do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -52,6 +52,8 @@ RSpec.configure do |config|
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
+  config.include Devise::TestHelpers, :type => :controller
+  # config.include ControllerHelpers, :type => :controller
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
